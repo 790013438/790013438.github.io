8a1bd11dbe3b9f248cc26d6b563369603113989f
790013438 <790013438@qq.com>
2017-10-17 19:12:31 +0800

    Disable jekyll functionality.

--- day4/scripts/5.js
+++
@@ -1,9 +0,0 @@
-var myButton = document.querySelector("#b5");
-myButton.onclick = function() {
-    var enterString = prompt("请输入一个字符串");
-    if (enterString === null || enterString == "") {
-        alert("字符串为空");
-    } else {
-        alert("字符串不为空");
-    }
-}
